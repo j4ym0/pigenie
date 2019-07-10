ba90bcfce3ea2fbaba21b2873b984dce75520d88
j4ym0 <jay_morrall@hotmail.com>
2019-07-10 13:29:04 +0100

    added run file to check for drivers and build if needed

---
+++ run.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+if [ ! -f src/energenie/drv/radio_rpi.so ]; then
+    echo "Drivers not found!"
+    chmod 755 src/energenie/drv/build_rpi
+    cd src/energenie/drv/
+    echo "Starting to build drivers"
+    ./build_rpi
+    cd ../..
+fi
+if [ ! -f /tmp/foo.txt ]; then
+  echo "Drivers found, starting monitor"
+  python3 src/monitor.py
+fi
