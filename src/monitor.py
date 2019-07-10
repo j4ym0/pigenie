032918a66553e231753a0087cd760c532fa01533
j4ym0 <jay_morrall@hotmail.com>
2019-07-10 17:51:33 +0100

    add smothing

--- src/monitor.py
+++ src/monitor.py
@@ -29,7 +29,7 @@ def energy_monitor_loop():
                 # work out the power factor for later
                 pf = ((d.get_current()/240)*d.get_apparent_power())
                 # working out the watts = amps x volts
-                GENORATION = round(240*d.get_current())/(0.84) # device by power factor
+                GENORATION =(240*d.get_current())/(0.84) # device by power factor
                 # if less than 1 watt this can be 0 as may just be the power for the meater
                 # digital meaters can use up to 3wh
                 if GENORATION < 5:
@@ -43,7 +43,11 @@ def energy_monitor_loop():
             pass # Ignore
 
     supply = GENORATION - cfg.base_watts
+    if cfg.use_smoothing:
+        supply = smooth.average() - cfg.base_watts
 
+    supply = round(supply)
+    
     if supply > 0:
         for socket in cfg.legacy_sockets:
             print("Remaining Supply %sw" % supply)
