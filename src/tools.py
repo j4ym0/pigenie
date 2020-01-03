58b2f7a0fd2533a12919df5ba340d921ba3e5478
j4ym0 <jay_morrall@hotmail.com>
2020-01-03 16:34:58 +0000

    Add fastinit to average

--- src/tools.py
+++ src/tools.py
@@ -10,14 +10,21 @@ class Average(): # used to collect data and average over time or values
 
     def __init__(self, size=6):
         self.store_size = (size+1)
-        self.store = [0] * self.store_size # init array of the size + 1 for the average result
-        self.store[0] = 0 # init the average for later
-        self.type = 1   # 1 = number of values
-                        # 2 = TODO over time
+        self.store = [0] * self.store_size  # init array of the size + 1 for the average result
+        self.store[0] = 0                   # init the average for later
+        self.fastinit = True                # 1 = number of values
+        self.type = 1                       # 1 = number of values
+                                            # 2 = TODO over time
 
     def add(self, value): # add a result to the array
         del self.store[1] # delete the yongest value
         self.store.append(value) # add the value to the end
+
+        if fastinit: # if valuse is 0 then init with current value
+            for i in range(len(self.store)-1):
+                if self.store[i+1] == 0:
+                    self.store[i+1] = value
+            fastinit = False # turn off so is not called later
 
         a = 0
         for i in range(len(self.store)-1):
